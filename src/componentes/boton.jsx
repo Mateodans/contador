193bccea391abe0ace5701b21a443ddeaa7037f0
Mateodans <90775644+Mateodans@users.noreply.github.com>
2022-08-30 23:43:28 -0300

    "finalizacion de 2do proyecto"

--- src/componentes/boton.jsx
+++ src/componentes/boton.jsx
@@ -3,7 +3,7 @@ import '../stylesheets/boton.css'
 
 function Boton({ texto, esBotonDeClic, manejarClic }) {
     return (
-    <button className = { esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' } onClick={manejarClic}>
+    <button className = { esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' } onClick={ manejarClic }>
         { texto }
     </button>
     );
--- src/componentes/contador.jsx
+++ src/componentes/contador.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../stylesheets/contador.css'
 
 function Contador({ numClics }){
     return(
